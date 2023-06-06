71dbada595549b822bec1ef5f69d2c1470ec9e75
@k$hay <akshaynaik5787@gmail.com>
2023-06-06 23:47:58 +0530

    added controller to updated product quantity

--- controllers/api/v1/product_controller.js
+++ controllers/api/v1/product_controller.js
@@ -56,3 +56,50 @@ module.exports.allProduct = async (req, res) => {
 
     }
 }
+
+module.exports.updateQuantity = async (req, res) => {
+    try {
+        // fetching the products by id
+        let product = await Product.findById(req.params.id);
+
+        // if product is not found
+        if(!product) {
+            return res.status(404).json({
+                message: "Product not found...!"
+            });
+        }
+
+        // updating the quantity of the selected product
+        product.quantity += parseInt(req.query.number, 10);
+
+        if(product.quantity < 0) {
+            return res.status(404).json({
+                message: "product Quantity cannot be -ve"
+            })
+        }
+
+        let updateProduct = await product.save();
+        
+        // on success show the updated product
+        return res.status(200).json({
+            data: {
+                product: {
+                    name: updateProduct.name,
+                    quantity: updateProduct.quantity
+                }
+            },
+            message: "Product Updated!"
+        });
+
+        
+    } catch (err) {
+
+        console.log('error',err); //to display error in console
+
+        // throw error on failure 
+        return res.status(500).json({
+            message: "Error in updating Quantity"
+        });
+        
+    }
+}
