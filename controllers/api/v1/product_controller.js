8d07ec0e852ceeac1a19ade8f7adbe3cd345ba2e
@k$hay <akshaynaik5787@gmail.com>
2023-06-06 22:22:53 +0530

    displaying all details

--- controllers/api/v1/product_controller.js
+++ controllers/api/v1/product_controller.js
@@ -18,8 +18,41 @@ module.exports.createProduct = async (req, res) => {
         console.log('error',err); //to display error in console
 
         // throw error on failure 
-        return res.status(400).json({
+        return res.status(500).json({
             message: "Error in creating a new product"
         })
+    }
+}
+
+module.exports.allProduct = async (req, res) => {
+
+    try {
+
+        const products = await Product.find({});
+
+        if(products.length < 1) {
+            return res.status(200).json({
+                message: "No Products Found"
+            });
+        }
+
+        if(products) {
+            return res.status(200).json({
+                data: {
+                    Products: products
+                },
+                message: "These are the products in inventory"
+            });
+        }
+
+    } catch (err) {
+
+        console.log('error',err); //to display error in console
+
+        // throw error on failure 
+        return res.status(500).json({
+            message: "Error in fetching products"
+        })
+
     }
 }
--- routes/api/v1/product.js
+++ routes/api/v1/product.js
@@ -2,7 +2,9 @@ const express = require('express');  //importing express library
 const router = express.Router();
 const productController = require('../../../controllers/api/v1/product_controller'); //importing the product controller fro controllers
 
+router.get('/', productController.allProduct); // displaying all the products
 router.post('/create-product', productController.createProduct); //creating a new product
+
 
 module.exports = router;
 
